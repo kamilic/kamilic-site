cf070975e364de26913eecc129970e78a302ec42
kamilic <kamilic.work@outlook.com>
2018-05-24 00:01:44 +0800

    update sh

--- sh/build.sh
+++ sh/build.sh
@@ -3,6 +3,6 @@ echo "update apt.....";
 # update apt-get
 apt update;
 
-./home/kamilic-site/sh/set-nginx.sh;
-./home/kamilic-site/sh//set-node.sh;
-./home/kamilic-site/sh/clone-all-repos.sh;
+bash /home/kamilic-site/sh/set-nginx.sh;
+bash /home/kamilic-site/sh//set-node.sh;
+bash /home/kamilic-site/sh/clone-all-repos.sh;
