32a9b9154a1e052d20f2deb6ab9bd6312fa7f56e
root <root@Intel.localdomain>
2018-05-24 00:19:52 +0800

    update sh

--- sh/clone-all-repos.sh
+++ sh/clone-all-repos.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # will change to nodejs after.
+if [ ! -f projects ];
+then mkdir /home/kamilic-site/projects;
+fi
 
-mkdir /home/kamilic-site/projects;
-cd /home/kamilic/projects;
+cd /home/kamilic-site/projects;
 
 # git clone git@github.com:kamilic/kamilic-me.git kamilic-me;
 git clone git@github.com:kamilic/my-hexo-blog-source.git kamilic-me-darkroom;
