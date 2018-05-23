79e468843c93e53e806f34c2475932c5c8177c52
root <root@Intel.localdomain>
2018-05-24 00:30:00 +0800

    git clone sh update

--- sh/clone-all-repos.sh
+++ sh/clone-all-repos.sh
@@ -7,5 +7,5 @@ fi
 cd /home/kamilic-site/projects;
 
 # git clone git@github.com:kamilic/kamilic-me.git kamilic-me;
-git clone git@github.com:kamilic/my-hexo-blog-source.git kamilic-me-darkroom;
+git clone git@github.com:kamilic/my-hexo-blog.git kamilic-me-darkroom;
 
