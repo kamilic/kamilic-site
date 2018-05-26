73f1c0423c87a3fd9f77bb2e144ae6dbadc5ef23
kamilic <kamilic.work@outlook.com>
2018-05-26 22:59:40 +0800

    update sh

--- sh/clone-all-repos.sh
+++ sh/clone-all-repos.sh
@@ -5,6 +5,10 @@ absolutePath=$(pwd);
 cd ..;
 repoRootPath=$(pwd);
 
+echo '=-=-=-=-= install kamilic-site repo dependencies =-=-=-=-=';
+cd ${repoRootPath};
+npm install;
+
 echo "=-=-=-=-= clone all projects repo =-=-=-=-=";
 mkdir -pv ${repoRootPath}/projects;
 node ${repoRootPath}/scripts/pull-repo.js --all;
--- sh/install-dependencies.sh
+++ sh/install-dependencies.sh
@@ -5,10 +5,6 @@ absolutePath=$(pwd);
 cd ..;
 repoRootPath=$(pwd);
 
-echo '=-=-=-=-= install kamilic-site repo dependencies =-=-=-=-=';
-cd ${repoRootPath};
-npm install;
-
 echo '=-=-=-=-= install kamilic-site-server repo dependencies =-=-=-=-=';
 cd ${repoRootPath}/server;
 npm install;
