75bc902afdfd892d7480e0fc0ba10923a58d0f50
kamilic <kamilic.work@outlook.com>
2018-05-26 23:57:43 +0800

    add some sh

--- sh/clone-all-repos.sh
+++ sh/clone-all-repos.sh
@@ -12,11 +12,3 @@ npm install;
 echo "=-=-=-=-= clone all projects repo =-=-=-=-=";
 mkdir -pv ${repoRootPath}/projects;
 node ${repoRootPath}/scripts/pull-repo.js --all;
-
-echo "=-=-=-=-= clone kamilic-site-server repo =-=-=-=-=";
-mkdir -pv ${repoRootPath}/server;
-cd ${repoRootPath}/server;
-git init;
-git remote add origin https://github.com/kamilic/kamilic-site-server;
-git pull origin master;
-
---
+++ sh/update-server-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+relativePath=$(dirname $0);
+cd ${relativePath}; # in /sh/
+absolutePath=$(pwd);
+cd ..;
+repoRootPath=$(pwd);
+
+echo "=-=-=-=-= clone kamilic-site-server repo =-=-=-=-=";
+mkdir -pv ${repoRootPath}/server;
+cd ${repoRootPath}/server;
+git init;
+git remote add origin https://github.com/kamilic/kamilic-site-server;
+git pull origin master;
+
