146c6d528250118b8463e28fb5209b025a2d59e8
root <root@Intel.localdomain>
2018-08-05 18:58:20 +0800

    update set-rc-local.sh

--- sh/set-rc-local.sh
+++ sh/set-rc-local.sh
@@ -9,5 +9,5 @@ cat ${repoRootPath}/sh/running-script.sh > /etc/init.d/env;
 
 vim /etc/init.d/env +;
 
-chmod 755 /etc/init.d/env;
-update-rc.d env defaults 95;
+chmod +x /etc/init.d/env;
+update-rc.d env defaults;
