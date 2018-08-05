64cb4f7d306d57574a63ab69cf79c8333d455e3d
kamilic <kamilic.work@outlook.com>
2018-08-05 18:43:59 +0800

    .\sh\running-script.sh

--- sh/running-script.sh
+++ sh/running-script.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 ### BEGIN INIT INFO
 # Provides: kamilic
-# Required-Start: $local_fs $network  
-# Required-Stop: $local_fs   
+# Required-Start: $local_fs $network 
+# Required-Stop: $local_fs
 # Default-Start: 2 3 4 5
 # Default-Stop:  0 1 6
 # Short-Description: kamilic
 # Description: kamilic       
 ### END INIT INFO
 # set-env
-export build_token='';
-cd /home/kamilic-site/;
-npm run start;
+case "$1" in
+        start)
+                export build_token='jef1314lic';
+                export npm="/usr/bin/npm";
+                export ssserver="/usr/local/bin/ssserver";
+                cd /home/kamilic-site/;
+                npm run start > /home/log/running-status;
+                ssserver -c /home/ss-config.json -d start;
+        ;;
+        stop)
+                #no-op
+        ;;
+
+        *)
+                #no-op
+        ;;
+esac
+
+exit 0;
